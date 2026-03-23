a9cb317f56162c4ab91bb366098d94681bd3fa70
Fdani007 <danieleliesel@gmail.com>
2026-03-23 12:37:58 -0400

    Update scanner.js

--- js/scanner.js
+++ js/scanner.js
@@ -1,1 +1,14 @@
+function iniciarEscaner(callbackSuccess) {
+    const html5QrCode = new Html5Qrcode("reader");
+    const config = { fps: 10, qrbox: { width: 250, height: 150 } };
 
+    html5QrCode.start(
+        { facingMode: "environment" }, 
+        config, 
+        (decodedText) => {
+            html5QrCode.stop(); // Detenemos la cámara tras leer
+            callbackSuccess(decodedText);
+            document.getElementById('reader').style.display = 'none';
+        }
+    );
+}
